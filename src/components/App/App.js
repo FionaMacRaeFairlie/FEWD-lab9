1da1de04e7e91791a3fbf3f77f51c583cda3b00d
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-11-07 08:58:05 +0000

    files for lab 9

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Preferences from '../Preferences/Preferences';
+import useToken from './useToken';
+
+function App() {
+
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+ 
+    return (
+      <>
+        <div className="wrapper">
+          <BrowserRouter>
+            <Routes>
+             <Route path="/" element={<h1>Application</h1>} />
+              <Route path="/dashboard"  element={<Dashboard />} />
+              <Route path="/preferences" element={<Preferences /> } />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </>
+    );
+  }
+  
+  export default App;
